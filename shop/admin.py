222427b21ef462b9a71f2ab2f61b997f4802cffc
phamdai <daipham952@gmail.com>
2022-04-08 14:01:10 +0700

    second commit

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Shop)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['id','name','shop']
+
+
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ['id','color','size','price','percent_discount','percent_discount_deal_shock','inventory','percent_discount_flash_sale']
+class CommentAdmin(admin.ModelAdmin):
+    list_display=['user','comment','parent']
+
+
+
+admin.site.register(Item,ItemAdmin)
+admin.site.register(Variation,VariationAdmin)
+admin.site.register(Color)
+admin.site.register(Size)
+admin.site.register(Buy_more_discount)
+admin.site.register(Comment,CommentAdmin)
+admin.site.register(Byproductcart)
+admin.site.register(IpModel)
+admin.site.register(UploadItem)
+
