63d532e79b3dc65da9a61632faf4d4ee38aaf43a
phamdai <daipham952@gmail.com>
2022-04-08 22:11:48 +0700

    second commit

--- discount/admin.py
+++ discount/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+admin.site.register(Buy_with_shock_deal)
+admin.site.register(Vocher)
+admin.site.register(Flash_sale)
+admin.site.register(Follower_offer)
+admin.site.register(Promotion_combo)
+admin.site.register(Shop_award)
+admin.site.register(Shop_program)
