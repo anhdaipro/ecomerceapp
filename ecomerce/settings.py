5fd513d8d640833f7430c6997a5307edcafb0f74
phamdai <daipham952@gmail.com>
2022-04-13 06:14:48 +0700

    first commit

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    
+    'django.contrib.staticfiles',
     'cloudinary_storage',
     'cloudinary',
     'social_django',
