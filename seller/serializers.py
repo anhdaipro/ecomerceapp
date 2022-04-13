0ba938ca8b23d990febc1495bc2d585841f4a15a
phamdai <daipham952@gmail.com>
2022-04-13 21:39:14 +0700

    first commit

--- seller/serializers.py
+++ seller/serializers.py
@@ -8,7 +8,7 @@ class VoucherSerializer(serializers.ModelSerializer):
     count_product=serializers.SerializerMethodField()
     class Meta:
         model = Vocher
-        fields = ['code_type','name_of_the_discount_program','code','valid_from','valid_to',
+        fields = ['id','code_type','name_of_the_discount_program','code','valid_from','valid_to',
                 'amount','percent','maximum_usage','number_used','count_product']
         read_only_fields = ['code_type']
     def get_number_used(self,obj):
@@ -20,7 +20,7 @@ class ComboSerializer(serializers.ModelSerializer):
     list_product=serializers.SerializerMethodField()
     class Meta:
         model = Promotion_combo
-        fields = ['promotion_combo_name','from_valid','to_valid','combo_type','list_product',
+        fields = ['id','promotion_combo_name','from_valid','to_valid','combo_type','list_product',
             'discount_percent','discount_price','price_special_sale','quantity_to_reduced']
     def get_list_product(self,obj):
         return [{'image':item.media_upload.all()[0].upload_file()} for item in obj.product.all()]
@@ -29,7 +29,7 @@ class ProgramSerializer(serializers.ModelSerializer):
     list_product=serializers.SerializerMethodField()
     class Meta:
         model = Shop_program
-        fields = ['name_program','valid_from','valid_to','list_product']
+        fields = ['id','name_program','valid_from','valid_to','list_product']
 
     def get_list_product(self,obj):
         return [{'image':item.media_upload.all()[0].upload_file()} for item in obj.product.all()]
@@ -39,7 +39,7 @@ class DealsockSerializer(serializers.ModelSerializer):
     list_byproduct=serializers.SerializerMethodField()
     class Meta:
         model = Buy_with_shock_deal
-        fields = ['shock_deal_type','program_name_buy_with_shock_deal','valid_from','valid_to',
+        fields = ['id','shock_deal_type','program_name_buy_with_shock_deal','valid_from','valid_to',
         'list_mainproduct','list_byproduct']
     def get_list_byproduct(self,obj):
         return [{'image':item.media_upload.all()[0].upload_file()} for item in obj.product.all()]
@@ -50,7 +50,7 @@ class FlashsaleSerializer(serializers.ModelSerializer):
     list_product=serializers.SerializerMethodField()
     class Meta:
         model = Flash_sale
-        fields = ['valid_from','valid_to','list_product']
+        fields = ['id','valid_from','valid_to','list_product']
         
     def get_list_product(self,obj):
         return [{'image':item.media_upload.all()[0].upload_file()} for item in obj.product.all()]
