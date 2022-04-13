74f8a6b4b70d2f148f3d69b31772887490cacc2b
phamdai <daipham952@gmail.com>
2022-04-13 13:04:28 +0700

    first commit

--- seller/views.py
+++ seller/views.py
@@ -39,6 +39,7 @@ from rest_framework.generics import (
     ListAPIView, RetrieveAPIView, CreateAPIView,
     UpdateAPIView, DestroyAPIView
 )
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from .serializers import VoucherSerializer
