ab799f898452e23839f33f9ce007ef5ec3153987
phamdai <daipham952@gmail.com>
2022-04-14 15:32:32 +0700

    first commit

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,7 +2,7 @@ import json
 from channels.consumer import AsyncConsumer
 from channels.db import database_sync_to_async
 from django.contrib.auth import get_user_model
-from chatapp.models import Thread,Message,UploadFile
+from chat.models import *
 User = get_user_model()
 from shop.models import *
 from checkout.models import *
