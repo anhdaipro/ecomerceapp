a590dd8aaa7e3fd9f8837b0a4bbca7a3526a8411
phamdai <daipham952@gmail.com>
2022-04-14 15:37:28 +0700

    first commit

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+class AddressAdmin(admin.ModelAdmin):
+    list_display  = ['id','user','name','address_type']
+admin.site.register(OrderItem,CartAdmin)
--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.utils import timezone
+
+# Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id','user','ref_code','shop','ordered','being_delivered',
+    'canceled','received','refund_requested','refund_granted','shipping_address'
+    ,'ordered_date']
+    list_display_links = ['user','shipping_address']
+    list_filter = ['ordered','being_delivered','received','refund_requested','refund_granted']
+    search_fields = ['user__username','ref_code']
+
+    actions = ['set_order_received','set_order_being_delivered']
+
+    def set_order_received(self,request,queryset):
+        queryset.update(received=True,received_date=timezone.now())
+
+    def set_order_being_delivered(self,request,queryset):
+        queryset.update(being_delivered=True)
+class CartAdmin(admin.ModelAdmin):
+    list_display  = ['id','user','product','quantity',]
+
+
+
+    
+
+admin.site.register(Order,OrderAdmin)
+admin.site.register(Address,AddressAdmin)
