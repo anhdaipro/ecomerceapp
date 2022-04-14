199198ddfed0e2980338b27095a6506b2d44f431
phamdai <daipham952@gmail.com>
2022-04-14 15:44:59 +0700

    first commit

--- cart/admin.py
+++ cart/admin.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import *
-class AddressAdmin(admin.ModelAdmin):
-    list_display  = ['id','user','name','address_type']
-admin.site.register(OrderItem,CartAdmin)
+
+admin.site.register(OrderItem)
