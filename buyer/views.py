dd20902619523e2860e970cb771e9e19d96fb8f1
phamdai <daipham952@gmail.com>
2022-04-17 21:31:32 +0700

    first commit

--- buyer/views.py
+++ buyer/views.py
@@ -66,9 +66,9 @@ class RegisterView(APIView):
 
 class LoginView(APIView):
     def post(self, request):
-        username = request.data['username']
-        password = request.data['password']
-        token=request.data['token']
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        token=request.POST.get('token')
         if token:
             token = AccessToken.objects.get(token=token)
             
