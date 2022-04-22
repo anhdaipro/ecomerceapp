20f8804781d97145c64e43ea632646286af358f9
phamdai <daipham952@gmail.com>
2022-04-22 08:18:13 +0700

    first commit

--- buyer/views.py
+++ buyer/views.py
@@ -3,6 +3,10 @@
 from twilio.rest import Client
 from django.db.models import Q
 from django.conf import settings
+from django.utils.http import urlsafe_base64_encode
+
+from django.utils.encoding import force_bytes
+
 from django.contrib.auth.tokens import default_token_generator
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404
