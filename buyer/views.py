1239f4c99ee2cb4899a14cb98f8f027eb422b6b8
phamdai <daipham952@gmail.com>
2022-04-22 08:23:39 +0700

    first commit

--- buyer/views.py
+++ buyer/views.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.utils.http import urlsafe_base64_encode
 
 from django.utils.encoding import force_bytes
-
+from django.utils.encoding import smart_str, force_str, smart_bytes, DjangoUnicodeDecodeError
 from django.contrib.auth.tokens import default_token_generator
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404
