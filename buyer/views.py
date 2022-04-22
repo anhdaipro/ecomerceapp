51e8abce142eaf3686d22588751f66ae4dc7a53c
phamdai <daipham952@gmail.com>
2022-04-22 19:00:17 +0700

    first commit

--- buyer/views.py
+++ buyer/views.py
@@ -2013,7 +2013,7 @@ class PasswordResetView(APIView):
 
 class SetNewPasswordAPIView(generics.GenericAPIView):
     serializer_class = SetNewPasswordSerializer
-    def patch(self, request):
+    def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         return Response({'success': True, 'message': 'Password reset success'}, status=status.HTTP_200_OK)
