9770d383357e08000e110b3e7c927c56d0b94c52
phamdai <daipham952@gmail.com>
2022-04-22 07:42:56 +0700

    first commit

--- buyer/send_email.py
+++ buyer/send_email.py
@@ -32,7 +32,7 @@ def send_register_mail(self, user, key):
         raise self.retry(exc=e, countdown=25200)
 
 
-def send_reset_password_email(user):
+def send_reset_password_email(self,user):
     body = """
     hello %s,
     reset url : %sretypepassword/%s/%s
