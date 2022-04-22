8855d35e075e4a5b8e5bcbbd7ad88fff8110f70d
phamdai <daipham952@gmail.com>
2022-04-22 21:04:50 +0700

    first commit

--- buyer/views.py
+++ buyer/views.py
@@ -161,9 +161,11 @@ class LoginView(APIView):
             return Response(data)
         
         else:
-            if username:
-                user = authenticate(request, email=email, password=password)
             user = authenticate(request, username=username, password=password)
+            if email:
+                user = authenticate(request, email=email, password=password)
+            uidb64 = urlsafe_base64_encode(smart_bytes(user.id))
+            token = default_token_generator.make_token(user)
             absurl = 'http://localhost:3000/forgot_password/' +uidb64+ '/'+token+'?email='+email
             email_body =f"Xin chao {user.username}, \nAi đó đang cố gắng truy cập Tài khoản của bạn.\nNếu Bạn đang thực hiện đăng nhập, vui lòng xác nhận TẠI ĐÂY (hiệu lực trong vòng 10 phút). \n{absurl}"
             data = {'email_body': email_body, 'to_email': user.email,
