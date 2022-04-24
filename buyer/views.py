4e1b27610721246b7b13c87f11fb89e2a4c5dc7b
phamdai <daipham952@gmail.com>
2022-04-24 11:40:29 +0700

    first commit

--- buyer/views.py
+++ buyer/views.py
@@ -465,8 +465,8 @@ class Topsearch(APIView):
         result_category = dict((i, list_title_category.count(i)) for i in list_title_category)
         list_sort_item={k: v for k, v in sorted(result_item.items(), key=lambda item: item[1],reverse=True)}
         list_sort_category={k: v for k, v in sorted(result_category.items(), key=lambda item: item[1],reverse=True)}
-        list_name_search_trend=sorted(list_sort_category, key=list_sort.get, reverse=True)[from_item:to_item]
-        list_name_top_search=sorted(list_sort_item, key=list_sort.get, reverse=True)[:20]
+        list_name_search_trend=sorted(list_sort_category, key=list_sort_category.get, reverse=True)[from_item:to_item]
+        list_name_top_search=sorted(list_sort_item, key=list_sort_item.get, reverse=True)[:20]
         item_search_trend=Category.objects.filter(Q(name__in=list_name_search_trend))
         item_top_search=Item.objects.filter(Q(name__in=list_name_top_search))
         data={'item_search_trend':[{'title':category.title,'count':get_count(category),'image':category.item_set.all()[0].get_image_cover()} for category in item_search_trend],
