fb90fda103ab59eaa4d92e4a61b17bc911c37273
phamdai <daipham952@gmail.com>
2022-04-25 09:14:53 +0700

    first commit

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    
+    list_display=['user','phone']
+admin.site.register(Profile,ProfileAdmin)
 admin.site.register(SMSVerification)
