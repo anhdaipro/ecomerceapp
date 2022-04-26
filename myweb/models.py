eb3f43ceeee5ff96735b40a30cfbd7575f682fb2
phamdai <daipham952@gmail.com>
2022-04-26 10:46:36 +0700

    first commit

--- myweb/models.py
+++ myweb/models.py
@@ -5,6 +5,7 @@ class Notify(models.Model):
     customer=models.ManyToManyField(User,blank=True)
     image=models.ImageField(null=True)
     message=models.CharField(max_length=1000)
+    read=models.BooleanField(default=False)
 class Image_home(models.Model):
     image=models.ImageField(null=True)
     url_field=models.URLField(max_length=200,null=True)
