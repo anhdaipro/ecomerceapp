46a96dd1f71c1b097b5fc421370123eb71d2eb4f
phamdai <daipham952@gmail.com>
2022-04-28 18:40:28 +0700

    first commit

--- myweb/models.py
+++ myweb/models.py
@@ -12,7 +12,7 @@ class Image_home(models.Model):
     url_field=models.URLField(max_length=200,null=True)
     def get_image(self):
         if self.image and hasattr(self.image,'url'):
-            retunr self.image.url
+            return self.image.url
 class SearchKey(models.Model):
     keyword = models.CharField(max_length=255)
     total_searches = models.IntegerField(default=0)
