54724369ba2f6397bfd1c0e63b204a379d7255d3
phamdai <daipham952@gmail.com>
2022-05-07 09:47:16 +0700

    first commit

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,14 +2,14 @@ import json
 from channels.consumer import AsyncConsumer
 from channels.db import database_sync_to_async
 from django.contrib.auth import get_user_model
-
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from chat.models import *
 User = get_user_model()
 from shop.models import *
 from checkout.models import *
 from django.utils import timezone
 
-class ChatConsumer(AsyncConsumer):
+class ChatConsumer(AsyncJsonWebsocketConsumer):
     async def websocket_connect(self, event):
         print('connected', event)
         user = self.scope['user']
@@ -171,7 +171,7 @@ class ChatConsumer(AsyncConsumer):
                                 message.file.add(files)
         return count
     @database_sync_to_async
-	def update_user_status(self, user,status):
+    def update_user_status(self, user,status):
 		return Profile.objects.filter(user=user).update(online=status)
    
 
