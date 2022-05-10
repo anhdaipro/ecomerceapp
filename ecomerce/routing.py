6756658fa68a92fb8ae7464fe3696af4b3705b9f
phamdai <daipham952@gmail.com>
2022-05-10 16:33:53 +0700

    first commit

--- ecomerce/routing.py
+++ ecomerce/routing.py
@@ -2,10 +2,10 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
-from .json_token_auth import JwtAuthMiddleware
+
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
-    'websocket': JwtAuthMiddleware(
+    'websocket': AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
         )
