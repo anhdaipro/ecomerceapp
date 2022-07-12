4489d087d7a4b626516c759559f4fba376fa1bcf
phamdai <daipham952@gmail.com>
2022-07-12 23:28:45 +0700

    make it better

--- chats/views.py
+++ chats/views.py
@@ -104,14 +104,14 @@ class ActionThread(APIView):
 class CountThread(APIView):
     permission_classes = (IsAuthenticated,)
     def get(self,request):
-        count=Thread.objects.filter(participants=request.user).exclude(message_thread=None).count()
+        count=Thread.objects.filter(participants=request.user).exclude(chatmessage_thread=None).count()
         return Response({'count':count})
 
 class ListThreadAPIView(APIView):
     permission_classes = (IsAuthenticated,)
     def get(self,request):
         user=request.user
-        threads=Thread.objects.filter(participants=user).exclude(message_thread=None)
+        threads=Thread.objects.filter(participants=user).exclude(chatmessage_thread=None)
         serializer = ThreadinfoSerializer(threads,many=True, context={"request": request})
         return Response(serializer.data)
 
