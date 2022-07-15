095ef2993b2019f5c2f95dcba5fa0e2ea9b1bec5
phamdai <daipham952@gmail.com>
2022-07-15 16:03:49 +0700

    first commit

--- chats/views.py
+++ chats/views.py
@@ -73,7 +73,7 @@ class ActionThread(APIView):
                 data={'count_order':count_order,
                     'list_orders':[{
                     'id':order.id,'total_final_order':order.total_final_order(),
-                    'count_item':order.count_item_cart(),
+                    'count_item':order.count_item_cart(),'shop_id':order.shop_id,
                     'cart_item':[{'item_image':cartitem.get_image(),'item_url':cartitem.product.item.get_absolute_url(),
                     'item_name':cartitem.product.item.name,'color_value':cartitem.product.get_color(),
                     'quantity':cartitem.quantity,'discount_price':cartitem.product.total_discount(),
