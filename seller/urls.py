a7310118d3812d37ee0531640e552cc239f64ca0
phamdai <daipham952@gmail.com>
2022-07-24 22:48:40 +0700

    first commit

--- seller/urls.py
+++ seller/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
 from .views import (DashboardBundleAPI,DashboardVoucherAPI,DashboardAddonAPI,
-DashboardFlashsaleAPI,DashboardDiscountAPI,
-DashboardDiscount,DataBundleAPI,
+DashboardFlashsaleAPI,DashboardDiscountAPI,DataBundleAPI,
 DataVoucherAPI,
 DataDiscountAPI,
 DataFlashsaleAPI,
