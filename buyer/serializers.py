bf4c895215e751e79355c9fcbf8b87c3195c1e26
phamdai <daipham952@gmail.com>
2022-10-06 08:40:10 +0700

    first commit

--- buyer/serializers.py
+++ buyer/serializers.py
@@ -882,14 +882,21 @@ class ReviewSerializer(serializers.ModelSerializer):
 class ReviewitemSerializer(ReviewSerializer):
     user = serializers.SerializerMethodField()
     shop=serializers.SerializerMethodField()
+    liked=serializers.SerializerMethodField()
     class Meta(ReviewSerializer.Meta):
-        fields=ReviewSerializer.Meta.fields+['user','shop']
+        fields=ReviewSerializer.Meta.fields+['user','shop','liked']
     def get_user(self,obj):
         return UserorderSerializer(obj.user).data
     def get_shop(self,obj):
         if obj.user.shop:
             return obj.user.shop.get_absolute_url()
-
+    def get_liked(self,obj):
+        liked=False
+        request=self.context.get("request")
+        token = request.META.get('HTTP_AUTHORIZATION', " ").split(' ')[1]
+        if token and request.user in obj.like.all():
+            liked=True
+        return liked
 class ReviewshopSerializer(ReviewSerializer):
     user = serializers.SerializerMethodField()
     reply = serializers.SerializerMethodField()
